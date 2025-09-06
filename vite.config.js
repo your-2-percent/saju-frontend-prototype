e96209ecab72eaade042b18926f1a83667c43b3a
nahyunee96 <unique5264@naver.com>
2025-09-07 00:00:02 +0900

    chore: 배포 설정하고 gitignore 수정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 export default defineConfig({
     plugins: [react()],
-    resolve: { alias: { '@': path.resolve(__dirname, 'src') } }
+    resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+    base: '/'
 });
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, 'src') } }
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+  base: '/'
 });
