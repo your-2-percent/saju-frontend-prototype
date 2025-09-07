3bc82f1e6767b0471ae980e4d8d557c7bbf0b034
nahyunee96 <108210796+nahyunee96@users.noreply.github.com>
2025-09-08 06:09:09 +0900

    feat: 푸터생성 및 간지 크기 변경 (#6)

---
+++ src/app/pages/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-8 px-4 border-t border-neutral-700 ">
+      <p className="text-center text-[12px] text-orange-300">Copyright © 2025 Hwarim96. All rights reserved.</p>
+    </footer>
+  )
+}
