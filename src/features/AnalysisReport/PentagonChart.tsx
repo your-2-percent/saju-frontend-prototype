6173c03d537345b67abeaf70429c155e830c67f2
nahyunee96 <unique5264@naver.com>
2025-09-11 18:47:05 +0900

    fix: 피커전역수정

--- src/features/AnalysisReport/PentagonChart.tsx
+++ src/features/AnalysisReport/PentagonChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import type { PowerData, TenGod } from "./utils/types";
 import { useLuckPickerStore } from "@/shared/lib/hooks/useLuckPickerStore";
 
