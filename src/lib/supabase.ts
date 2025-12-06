d1e247d371524b0c2def9dbeff80d88f3474fc48
nahyunee96 <unique5264@naver.com>
2025-12-06 16:13:36 +0900

    [SAJU-155] fix. 로그인좀돼라제발

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
 // lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
+console.log("ENV CHECK:", {
+  url: import.meta.env.VITE_SUPABASE_URL,
+  key: import.meta.env.VITE_SUPABASE_ANON_KEY,
+});
+
 export function getSupabaseClient() {
   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
   const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
