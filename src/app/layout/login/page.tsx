35acca792291ade83680b0c0484c12a7e9dea5a6
nahyunee96 <unique5264@naver.com>
2025-12-06 16:02:42 +0900

    [SAJU-154] fix. 로그인좀돼라제발

--- src/app/layout/login/page.tsx
+++ src/app/layout/login/page.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: `${window.location.origin}/index.html`,
       },
     });
 
