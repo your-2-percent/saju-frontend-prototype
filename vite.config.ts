847f7c5488adc3b316118812b70ad507b0952339
nahyunee96 <unique5264@naver.com>
2025-12-09 20:47:48 +0900

    [SAJU-206] fix. 관리자 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
-  base: '/'
+  // GitHub Pages에서 gh-pages 브랜치로 호스팅 시 저장소명을 base로 설정해야 경로가 깨지지 않습니다.
+  base: '/frontend-hwarim/'
 });
