ad9c07663a1c28c2194e546671e9d2626572d049
nahyunee96 <108210796+nahyunee96@users.noreply.github.com>
2025-12-09 21:50:31 +0900

    [SAJU-211] fix. 관리자 연결 (#208)

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
   // GitHub Pages에서 gh-pages 브랜치로 호스팅 시 저장소명을 base로 설정해야 경로가 깨지지 않습니다.
-  base: '/frontend-hwarim/'
+  base: '/'
 });
